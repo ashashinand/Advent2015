26ef11e2bb30d4332a376fead04fbe38948a3539
ashashinand <github@ashashinand.net>
2015-12-03 04:51:15 +1100

    ho ho ha ha day 1 xd

--- Advent2015/Program.cs
+++ Advent2015/Program.cs
@@ -1,11 +1,103 @@
 ﻿using System;
+using Advent2015.interfaces;
+using Advent2015.classes;
 
 namespace Advent2015
 {
     class Program
     {
+        //Program entry point
         static void Main(string[] args)
         {
+            //variables
+            int iChoice;
+            string sChoice = "";
+            IAdventProblem problem; 
+
+            //basically while(true) because theres a break but incase it fails somehow, generate a false
+            while(!(sChoice == "exit" || sChoice == "quit"))
+            {
+                iChoice = 0;
+                //Was it really worth avoiding hitting the = key 20 times?
+                Console.WriteLine("".PadRight(20, '='));
+                Console.WriteLine("Please input number for Advent Challenge Day:");
+                Console.Write(">");
+                sChoice = Console.ReadLine();
+
+                //Error handling
+                try
+                {
+                    iChoice = Convert.ToInt32(sChoice);
+                }
+                catch
+                {
+                    //Two legitmate reasons why its not a number
+                    if (sChoice == "exit" || sChoice == "quit")
+                        break;
+
+                    //otherwise yell at the user
+                    Console.WriteLine("Invalid number entered!");
+                }
+                finally
+                {
+                    //If we didn't work out, leave signs that it didn't
+                    if (iChoice == 0)
+                        iChoice = -1;
+                }
+
+                //Good to go? wew
+                if (iChoice >= 0)
+                {
+                    problem = null;
+
+                    //Switch case because I dont like chaining if statements
+                    switch(iChoice)
+                    {
+                        //HAHA
+                        case 0:
+                            Console.WriteLine("Jokes on you, its not zero indexed.");
+                            break;
+                        case 1:
+                            problem = new Day1();
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                        case 19:
+                        case 20:
+                        case 21:
+                        case 22:
+                        case 23:
+                        case 24:
+                        case 25:
+                            Console.WriteLine("Not done yet, check back later.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid day entered! Range is 1-25");
+                            break;
+                    }
+
+                    if (problem != null)
+                    {
+                        problem.Run();
+                    }
+                }
+
+            }
         }
     }
 }
---
+++ Advent2015/classes/Day1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using Advent2015.interfaces;
+using System.IO;
+
+namespace Advent2015.classes
+{
+    class Day1 : IAdventProblem
+    {
+        //variable setup
+        private int _iIncrementer;
+        private string _sInputString;
+
+        //I know the input string is about 8k but whatever
+        private const int MAX_INPUT_SIZE = 16384;
+
+        //haha what in the fuck fucking 254 character limit
+        //Time to circumvent with IO streams
+        private string ReadLine()
+        {
+            byte[] inputBuffer = new byte[MAX_INPUT_SIZE]; 
+            Stream inputStream = Console.OpenStandardInput(inputBuffer.Length);
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
+            return Console.ReadLine();
+        }
+
+        //Constructor
+        public Day1()
+        {
+            //default variable initialisation
+            _iIncrementer = 0;
+            _sInputString = "";
+        }
+
+        //"Entry" point
+        public int Run()
+        {
+            Console.WriteLine("--- Day 1: Not Quite Lisp ---");
+            Console.WriteLine("AKA: Look dad, archaic incrementer!");
+            Console.WriteLine("Please insert input string:");
+            Console.Write(">");
+            _sInputString = ReadLine();
+
+            //Do we have a string?
+            if (_sInputString == "")
+            {
+                Console.WriteLine("Yeah, thanks. Input a string next time.");
+                return -1;
+            }
+
+            //Run through the inputs character at a time
+            //I think this is the "best" way to do it but im not sure how fast string searching is
+            foreach (char cInput in _sInputString)
+            {
+                // ) is down 1 floor, ( is up one floor, anything else is crash the elevator
+                if (cInput == ')')
+                    _iIncrementer--;
+                else if (cInput == '(')
+                    _iIncrementer++;
+                else
+                {
+                    Console.WriteLine("Unknown character {0} entered!", cInput);
+                    return -1;
+                }
+            }
+
+            //And finally, output
+            Console.WriteLine("Final floor number is: {0}", _iIncrementer);
+            return 0;
+        }
+    }
+}
